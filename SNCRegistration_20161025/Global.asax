09a2c4f7334d38c40b611c3c5b1b6feb08514d90
Jennifer Stelmach <jstelmach@klsmartin.com>
2016-10-25 06:25:44 -0400

    Add project files.

---
+++ SNCRegistration_20161025/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SNCRegistration_20161025.MvcApplication" Language="C#" %>
---
+++ SNCRegistration_20161025/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SNCRegistration_20161025.Startup))]
+namespace SNCRegistration_20161025
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
